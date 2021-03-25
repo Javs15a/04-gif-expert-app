f72c5fcae8be027eca04413a34c803d9a029e3d4
Javier150296 <jvr.leon.orozco@gmail.com>
2021-03-24 23:15:34 -0600

    Update tests

--- src/tests/GifExpertApp.test.js
+++ src/tests/GifExpertApp.test.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 import { shallow } from "enzyme";
 import { GifExpertApp } from "../GifExpertApp";
 
-
-describe('Pruebas de <GifExperApp/>', () => {
-
-    test('debe de cargar el componente correctamente', () => {
-        const wrapper = shallow(<GifExpertApp />);
+describe("Pruebas de <GifExperApp/>", () => {
+    test("debe de cargar el componente correctamente", () => {
+        const wrapper = shallow( <GifExpertApp /> );
 
         expect(wrapper).toMatchSnapshot();
     });
 
+    test("debe de mostrar una lista de categorias", () => {
+        const categories = ["One punch", "Dragon ball"];
+        const wrapper = shallow( <GifExpertApp defaultCategories = { categories }/>);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length);
+    });
 });
